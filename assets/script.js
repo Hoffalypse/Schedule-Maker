de939cb78ccaebc100897cfad4915979b558fed1
Bryan Wienhoff <106857564+Hoffalypse@users.noreply.github.com>
2022-07-16 15:40:40 -0600

    add js file and time upload

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+
+//display current date on top of the page 
+
+const time = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+alert(time);
+//display timeblocks for working hours of the day 
+//color code time blocks for past present and future
+//enter info into each timeblock 
+//save timeblock info into local storage 
+// when page is refreshed, saved events persist 
